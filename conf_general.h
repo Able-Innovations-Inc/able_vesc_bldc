83eada5729656b4bd8edaef9f721073672f975bf
ryan.wang <ryan@ableinnovations.ca>
2024-07-25 14:00:25 -0400

    change to 75_300_r2

--- conf_general.h
+++ conf_general.h
@@ -74,8 +74,8 @@
 //#define HW60_IS_MK4
 #define HW60_IS_MK5
 
-#define HW_SOURCE "hw_60.c"
-#define HW_HEADER "hw_60.h"
+//#define HW_SOURCE "hw_60.c"
+//#define HW_HEADER "hw_60.h"
 
 //#define HW_SOURCE "hw_r2.c"
 //#define HW_HEADER "hw_r2.h"
@@ -102,11 +102,11 @@
 //#define HW75_300_VEDDER_FIRST_PCB
 
 // Second revision with separate UART for NRF51
-//#define HW75_300_REV_2
-#define HW75_300_REV_3
+#define HW75_300_REV_2
+//#define HW75_300_REV_3
 
-//#define HW_SOURCE "hw_75_300.c"
-//#define HW_HEADER "hw_75_300.h"
+#define HW_SOURCE "hw_75_300.c"
+#define HW_HEADER "hw_75_300.h"
 
 //#define HW_SOURCE "hw_mini4.c"
 //#define HW_HEADER "hw_mini4.h"
